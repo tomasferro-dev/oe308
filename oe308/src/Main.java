98860f80eb11044d11488131987a638bf77e8d28
tomasferro-dev <tomasferro.dev@gmail.com>
2024-08-30 16:10:08 -0300

    crear funcion suma

--- oe308/src/Main.java
+++ oe308/src/Main.java
@@ -4,7 +4,12 @@ public class Main {
     public static <T> void printea(T content) {
         System.out.println(content);
     }
+    public static void suma(int a, int b) {
+        printea("El resultado de la suma es: ");
+        printea(a + b);
+    }
     public static void main(String[] args) {
         printea("hola mundito");
+        suma(34, 324);
     }
 }
