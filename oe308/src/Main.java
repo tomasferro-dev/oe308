7e842a4006b538dc2343f9be91d2d2f5dfe522aa
tomasferro-dev <tomasferro.dev@gmail.com>
2024-08-30 16:26:32 -0300

    cambiar funcion suma por funcion resta

--- oe308/src/Main.java
+++ oe308/src/Main.java
@@ -4,12 +4,12 @@ public class Main {
     public static <T> void printea(T content) {
         System.out.println(content);
     }
-    public static void suma(int a, int b) {
+    public static void resta(int a, int b) {
         printea("El resultado de la suma es: ");
-        printea(a + b);
+        printea(a - b);
     }
     public static void main(String[] args) {
         printea("hola mundito");
-        suma(34, 324);
+        resta(324, 34);
     }
 }
